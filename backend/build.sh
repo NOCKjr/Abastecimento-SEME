f4792c10da6ffbffd20d1df8d9e84be3efb1313b
AlonsoMCDG <alonsomartins056@gmail.com>
2026-03-04 22:49:45 -0500

    remover o comando temporário para apagar o banco de dados antigo da ambiente de produção (Render)

--- backend/build.sh
+++ backend/build.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-
-python manage.py shell << END
-from django.db import connection
-cursor = connection.cursor()
-cursor.execute("DROP SCHEMA public CASCADE;")
-cursor.execute("CREATE SCHEMA public;")
-END
-
 python manage.py migrate
 python manage.py collectstatic --noinput
