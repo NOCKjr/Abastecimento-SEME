2604cab1b0b7b32f072b9fcb96a14e4e80d4d2c7
AlonsoMCDG <alonsomartins056@gmail.com>
2026-03-04 22:38:17 -0500

    adicionar comando para apagar o banco de dados antigo da ambiente de produção (Render)

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py makemigrations
+
+python manage.py shell << END
+from django.db import connection
+cursor = connection.cursor()
+cursor.execute("DROP SCHEMA public CASCADE;")
+cursor.execute("CREATE SCHEMA public;")
+END
+
 python manage.py migrate
 python manage.py collectstatic --noinput
